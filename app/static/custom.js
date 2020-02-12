8c47e9e14c8665a199695dbf0db3b2da36aafb99
Jonathan Dreyer <jonathan.dreyer@cleanenergie.ch>
2020-02-12 16:38:30 +0100

    Refresh data automatically (every 5 sec)

--- app/static/custom.js
+++ app/static/custom.js
@@ -1,7 +1,7 @@
 (function($){
 
     function update_numbers(confirmed, deaths, recovered) {
-        $('.data-virus').html('Currently,<br>there is ' + confirmed + ' confirmed, ' + deaths + ' deaths and ' + recovered + 'recovered.');
+        $('.data-virus').html('Currently,<br>there is ' + confirmed + ' confirmed, ' + deaths + ' deaths and ' + recovered + ' recovered.');
 	}
 
 	function update_timestamp(ts) {
@@ -21,11 +21,13 @@
                     update_numbers(data['confirmed'], data['deaths'], data['recovered'],);
                     update_timestamp(response['timestamp']);
                     },
-                complete: function() {
-                    setInterval(update_data, 5000);
+                error : function() {
+                    console.log('Error to fetch data!');
                 }
             });
-
         }
+
+        setInterval(update_data, 5000);
+
     }, false);
 })(jQuery);
