db96fbbbdb605894705a6601dd5cc133ac9ef084
Jonathan Dreyer <jonathan.dreyer@cleanenergie.ch>
2020-02-12 22:56:48 +0100

    Fix height

--- app/static/custom.js
+++ app/static/custom.js
@@ -37,8 +37,8 @@
 
     // Set the dimensions of the canvas / graph
     var	margin = {top: 30, right: 20, bottom: 30, left: 50},
-        width = 400 - margin.left - margin.right,
-        height = 220 - margin.top - margin.bottom;
+        width = $("#chartArea").width() - margin.left - margin.right,
+        height = $("#chartArea").height() - margin.top - margin.bottom;
 
     // Parse the date / time
     var	parseDate = d3.time.format("%d-%b-%y").parse;
